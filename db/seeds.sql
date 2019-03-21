b6062e25099c7c5e7b4121bea34e46a52e7add05
Purni <purnia92@gmail.com>
2019-03-21 01:11:38 -0500

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO cats (burger_name, devoured) VALUES ('Cheese burger', false);
+INSERT INTO cats (burger_name, devoured) VALUES ('Veggie burger', false);
+INSERT INTO cats (burger_name, devoured) VALUES ('Fish burger', false);
